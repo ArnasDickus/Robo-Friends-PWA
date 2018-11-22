9029a57cc0925831073f67f69ff93b8fa169e8ef
Arnas <1arnasdickus1@gmail.com>
2018-11-22 14:10:14 +0200

    Updates

---
+++ precache-manifest.32de04610151c1fd50da3d14fc928418.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/Robo-Friends-PWA/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "c07431beb627be2df051",
+    "url": "/Robo-Friends-PWA/static/js/runtime~main.c07431be.js"
+  },
+  {
+    "revision": "f25a70ca1ba6e66eb9e4",
+    "url": "/Robo-Friends-PWA/static/js/main.f25a70ca.chunk.js"
+  },
+  {
+    "revision": "01ee3cd00f8151a1a8b5",
+    "url": "/Robo-Friends-PWA/static/js/1.01ee3cd0.chunk.js"
+  },
+  {
+    "revision": "f25a70ca1ba6e66eb9e4",
+    "url": "/Robo-Friends-PWA/static/css/main.6deadd53.chunk.css"
+  },
+  {
+    "revision": "01ee3cd00f8151a1a8b5",
+    "url": "/Robo-Friends-PWA/static/css/1.18ecc1ae.chunk.css"
+  },
+  {
+    "revision": "81904ee5ba4fbff1a929532484e86c73",
+    "url": "/Robo-Friends-PWA/index.html"
+  }
+];
